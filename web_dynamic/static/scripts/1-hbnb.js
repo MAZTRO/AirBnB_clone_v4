0979ca4ceb26227b97d627fd2d483c67cd9d604e
evertcolombia <evertescalante@gmail.com>
2020-05-15 23:06:01 +0000

    adding reactive content for amenities in the correct space

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,27 @@
+const amenities = [];
+  
+function checked () {
+  const name =  $(this).attr('data-name');
+  if (($(this).is(':checked')) {
+    amenities.push(name);
+  } else {
+    deleteElement(name);
+  }
+  showAmenities(array);
+}
+
+function deleteElement (name) {
+  for (let i = 0; i < amenities.length; i++) {
+    if (amenities[i] === name) {
+      amenities.splice(i, 1);
+    }
+  }
+}
+
+function showAmenities (array) {
+    $('DIV.amenities h4').text(array.join(', '));
+}
+
+$(document).ready(function() {
+  $('DIV.amenities .popover UL LI input').on('change', checked);
+});
