60912705da4759435796d93526d72a35b56f36b3
MAZTRO <design.mallet@gmail.com>
2020-05-16 18:37:34 +0000

    Finish task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,46 @@
+const amenities = [];
+
+function checked () {
+  const name =  $(this).attr('data-name');
+  if ($(this).is(':checked')) {
+    amenities.push(name);
+  } else {
+    deleteElement(name);
+  }
+  showAmenities(amenities);
+}
+
+function deleteElement (name) {
+  for (let i = 0; i < amenities.length; i++) {
+    if (amenities[i] === name) {
+      amenities.splice(i, 1);
+    }
+  }
+}
+
+function showAmenities (array) {
+    $('DIV.amenities h4').text(array.join(', '));
+}
+
+$(document).ready(function() {
+  $('DIV.amenities .popover UL LI input').on('change', checked);
+
+  /* $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: function (data) {
+      console.log(data);
+    }
+  }); */
+
+  $.get("http://localhost:5001/api/v1/status/", function(data)
+	{
+    console.log(data.status);
+    if (data.status === 'OK') {
+      $('#api_status').removeClass();
+      $('#api_status').toggleClass('available')
+    } else {
+      $('#api_status').removeClass();
+    }
+	});
+});
