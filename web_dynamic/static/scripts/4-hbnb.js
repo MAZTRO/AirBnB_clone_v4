b0e75462b64df164ed035069d866ffa75f73af9d
MAZTRO <design.mallet@gmail.com>
2020-05-18 17:00:22 +0000

    Add task 5 and fix somethings of task 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,4 +1,6 @@
 const amenities = [];
+const amenity_id = [];
+let filter = {};
 
 function createPlaceTemplate(place) {
   return (
@@ -20,10 +22,13 @@ function createPlaceTemplate(place) {
 
 function checked () {
   const name =  $(this).attr('data-name');
+  const id = $(this).attr('data-id');
   if ($(this).is(':checked')) {
     amenities.push(name);
+    amenity_id.push(id);
   } else {
     deleteElement(name);
+    deleteId(id);
   }
   showAmenities(amenities);
 }
@@ -36,26 +41,35 @@ function deleteElement (name) {
   }
 }
 
+function deleteId (id) {
+  for (let i = 0; i < amenity_id.length; i++) {
+    if (amenity_id[i] === id) {
+      amenity_id.splice(i, 1);
+    }
+  }
+}
+
 function showAmenities (array) {
-    $('DIV.amenities h4').text(array.join(', '));
+  $('DIV.amenities h4').text(array.join(', '));
 }
 
 function generateTemplate(data) {
+  $('SECTION.places').empty();
   for (const x in data) {
-      console.log(data[x]);
       //generate the html template
       const template = createPlaceTemplate(data[x]);
       $('SECTION.places').append(template);
     }
 }
 
-function placesPostRequest() {
+function placesPostRequest(filter) {
   $.ajax({
     url: 'http://localhost:5001/api/v1/places_search/',
     contentType: 'application/json',
-    data: '{}',
+    data: `${filter}`,
     type: 'POST',
     success: function (data) {
+      console.log("number of items found:", data.length);
       console.log(data);
       generateTemplate(data);
     }
@@ -67,7 +81,7 @@ $(document).ready(function() {
 
   $.get("http://localhost:5001/api/v1/status/", function(data)
 	{
-    console.log(data.status);
+    console.log("Status of API:", data.status);
     if (data.status === 'OK') {
       $('#api_status').removeClass();
       $('#api_status').toggleClass('available')
@@ -76,5 +90,9 @@ $(document).ready(function() {
     }
 	});
 
-  placesPostRequest();
+  placesPostRequest(JSON.stringify(filter));
+  $('button').on('click', function () {
+    filter['amenities'] = amenity_id;
+    placesPostRequest(JSON.stringify(filter));
+  });
 });
